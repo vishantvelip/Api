f924963aec3c20b7a63a385ed143a89348829b5b
Vishantvelip <vishantvelip92@gmail.com>
2025-06-05 13:55:13 +0530

    Update app.js

--- api/app.js
+++ api/app.js
@@ -18,7 +18,7 @@ const app = express();
 
 // MongoDB connection
 mongoose
-  .connect(process.env.MONGO_URI || "mongodb://localhost:27017/evening123")
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
